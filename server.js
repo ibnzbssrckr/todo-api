90bf086cfa85d44485967ce64249921ecf2a8569
Michael Geissler <ibnzbssrckr@yahoo.com>
2016-05-22 08:18:39 -0500

    Add ?completed filter

--- server.js
+++ server.js
@@ -13,8 +13,22 @@ app.get('/', function(req, res) {
     res.send('Todo API Root');
 });
 
+// GET /todos?completed=true
 app.get('/todos', function(req, res) {
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos  = todos;
+
+    if(queryParams.hasOwnProperty('completed')) {
+        if (queryParams.completed === 'true') {
+            filteredTodos = _.where(filteredTodos, {completed: true});
+        } else if(queryParams.completed === 'false') {
+            filteredTodos = _.where(filteredTodos, {completed: false});
+        } else {
+            return res.status(400).send();
+        }
+    }
+
+    res.json(filteredTodos)
 });
 
 app.get('/todos/:id', function(req, res) {
