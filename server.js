ff20cf6aa191f9b3605184e73d703c5ced4a739f
Michael Geissler <ibnzbssrckr@yahoo.com>
2016-05-22 07:26:22 -0500

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -41,6 +41,34 @@ app.post('/todos', function(req, res){
     res.json(body);
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id : todoId});
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    var body = _.pick(req.body, 'description', 'completed');
+
+    var validAttributes = {};
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    } else if(body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description.trim();
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+
+    res.json(matchedTodo);
+});
+
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
