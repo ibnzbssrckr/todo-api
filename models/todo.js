cd93134e8dcab87cc769ae65b414aad878bee560
Michael Geissler <ibnzbssrckr@yahoo.com>
2016-05-22 11:28:59 -0500

    Add Todo model and POST /todos

---
+++ models/todo.js
@@ -0,0 +1,18 @@
+// This is predetermined format to use with sequelize.import
+
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('todo', {
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1,250]
+            }
+        },
+        completed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    });
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -49,16 +50,27 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
-
-    body.description = body.description.trim();
-
-    body.id = todoNextId++;
-    todos.push(body);
-
-    res.json(body);
+    db.Todo.create({
+        description: body.description.trim(),
+        completed: body.completed
+    }).then(
+        function(todo) { res.json(todo); }, 
+        function(e) { res.status(400).json(e); }
+    );
+    // call create on db.Todo
+    //  respond with 200 and todo
+    //  fail error e.toJSON() error
+    
+    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    //     return res.status(400).send();
+    // }
+    //
+    // body.description = body.description.trim();
+    //
+    // body.id = todoNextId++;
+    // todos.push(body);
+    //
+    // res.json(body);
 });
 
 // PUT /todos/:id
@@ -103,6 +115,9 @@ app.delete('/todos/:id', function(req, res) {
     }
 });
 
-app.listen(PORT, function() {
-   console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function() {
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });
+
